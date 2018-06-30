cb3740814cd30e8ef2308d3f056688fa187e6be0
skylark814 <skylark814@gmail.com>
2018-06-30 16:31:43 +0800

    20180630V3

--- src/benson02.js
+++ src/benson02.js
@@ -2,31 +2,48 @@ var Benson02Layer = cc.Layer.extend({
     sprite: null,
     sx: 1,
     sy: 1,
-    player1: new Array(13),
+    pokers: new Array(52),
+    cards: new Array(new Array(13), new Array(13), new Array(13), new Array(13)),
+    players: new Array(new Array(13), new Array(13), new Array(13), new Array(13)),
     ctor: function () {
 
         this._super();
 
-
+        this.shufflePoker();
         cc.spriteFrameCache.addSpriteFrames(
             res.poker_plist, res.poker_png
         );
         this.sprite = new cc.Sprite("#pokers_back.png");
-        this.sx = (cc.winSize.width / 13) / (this.sprite.width+24);
-        this.sy = (cc.winSize.height / 4) / (this.sprite.height+48);
+        this.sx = (cc.winSize.width / 13) / (this.sprite.width + 24);
+        this.sy = (cc.winSize.height / 4) / (this.sprite.height + 48);
 
-        for (let i = 0; i < this.player1.length; i++) {
-            this.player1[i] = new cc.Sprite("#pokers_back.png");
-            this.player1[i].x = cc.winSize.width * (i+1)/14;
-            this.player1[i].y = cc.winSize.height * 4 / 5;
-            this.player1[i].setScale(this.sx, this.sy);
-            this.addChild(this.player1[i]);
+
+        for (let j = 0; j < this.players.length; j++) {
+            for (let i = 0; i < this.players[j].length; i++) {
+                this.players[j][i] = new cc.Sprite("#pokers_"+this.cards[j][i]+".png");
+                this.players[j][i].x = cc.winSize.width * (i + 1) / 14;
+                this.players[j][i].y = cc.winSize.height * (j + 1) / 5;
+                this.players[j][i].setScale(this.sx, this.sy);
+                this.addChild(this.players[j][i]);
+            }
         }
 
 
-
-
         return true;
+    },
+    shufflePoker: function () {
+        for (let i = 0; i < this.pokers.length; i++) {
+            this.pokers[i] = i;
+        }
+        this.pokers = shuffle(this.pokers);
+        for(let i = 0;i<this.pokers.length;i++){
+            this.cards[i%4][parseInt(i/4)] = this.pokers[i];
+        }
+        this.cards[0].sort(function(a, b){return a - b});
+        this.cards[1].sort(function(a, b){return a - b});
+        this.cards[2].sort(function(a, b){return a - b});
+        this.cards[3].sort(function(a, b){return a - b});
+
     }
 });
 
@@ -38,3 +55,14 @@ var Benson02Scene = cc.Scene.extend({
     }
 });
 
+function shuffle(a) {
+    var i, j, x;
+
+    for (i = a.length; i; i--) {
+        j = parseInt(Math.random() * i);  // 0-9
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
